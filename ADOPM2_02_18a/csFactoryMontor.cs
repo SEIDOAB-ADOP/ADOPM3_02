c7ea7a4052ac4f2bb3faee6755e8f48919610693
Martin <martin_lenart@icloud.com>
2024-11-20 18:13:48 +0900

    initial commit

---
+++ ADOPM2_02_18a/csFactoryMontor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ADOPM3_02_18a
+{
+	public class csFactoryMontor
+	{
+		public Action<int> AlarmStatus { get; set; } = null;
+
+
+        public bool CheckStatus()
+		{
+			//kod to check the factory floor if any machine signals an error
+			//..
+			bool _allOk = false;
+			if (!_allOk)
+			{
+                //Signal an error
+                AlarmStatus?.Invoke(3);
+            }
+			else
+			{
+				//All good
+                AlarmStatus?.Invoke(0);
+            }
+
+			return _allOk;
+		}
+		public csFactoryMontor()
+		{
+		}
+	}
+}
+
